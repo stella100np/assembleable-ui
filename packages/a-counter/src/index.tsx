0333f2f88820f00309951a626ccac164830ac769
gyzeng <gyzeng@adjacentech.com>
2023-02-02 10:51:33 +0800

    🦄 refactor: 重构counter 组件

--- packages/a-counter/src/index.tsx
+++ packages/a-counter/src/index.tsx
@@ -9,87 +9,25 @@ import {
   InjectionKey,
   Ref,
   inject,
+  reactive,
 } from "vue";
 import { useCounter } from '@vueuse/core'
-import type { UseCounterOptions } from "@vueuse/core";
+import type { UseCounterOptions, } from "@vueuse/core";
 
-const incFnkey: InjectionKey<(delta?: number) => number> = Symbol('inc-key')
-const decFnKey: InjectionKey<(delta?: number) => number> = Symbol('dev-key')
+interface ICounter {
+  initialValue?: number,
+  options?: UseCounterOptions
+}
 
-
-export const ACounter = defineComponent({
+export const ACounter = defineComponent<ICounter>({
   name: "ACounter",
-  props: {
-    as: {
-      type: [String, Object] as PropType<HTMLElement>,
-      default: "div",
-    },
-    initialValue: {
-      type: Number,
-      default: 0
-    },
-    options: {
-      type: Object as PropType<UseCounterOptions>
-    }
-  },
   setup(props, { slots }) {
-    const { count, inc, dec, set, reset } = useCounter(props.initialValue, props.options)
-    provide(incFnkey, inc)
-    provide(decFnKey, dec)
+    const _data = useCounter(props.initialValue, props.options)
+    const data = reactive(_data)
 
-    return () => [
-      h('div', slots.default?.({ count })),
-    ]
-
+    return () => {
+      if (slots.default)
+        return slots.default(data)
+    }
   }
 });
-
-export const ACounterInc = defineComponent({
-  name: "ACounterInc",
-  props: {
-    as: {
-      type: [String, Object] as PropType<HTMLElement>,
-      default: "button",
-    },
-    delta: {
-      type: Number,
-      default: 1
-    },
-  },
-  setup(props, { slots }) {
-    const inc = inject(incFnkey)!
-
-    return () => [
-      h(props.as, {
-        onClick: () => {
-          inc(props.delta)
-        }
-      }, slots.default?.()),
-    ]
-  }
-})
-
-export const ACounterDec = defineComponent({
-  name: "ACounterDec",
-  props: {
-    as: {
-      type: [String, Object] as PropType<HTMLElement>,
-      default: "button",
-    },
-    delta: {
-      type: Number,
-      default: 1
-    },
-  },
-  setup(props, { slots }) {
-    const dec = inject(decFnKey)!
-
-    return () => [
-      h(props.as, {
-        onClick: () => {
-          dec(props.delta)
-        }
-      }, slots.default?.()),
-    ]
-  }
-})
